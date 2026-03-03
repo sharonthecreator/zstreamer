d60f02274f7f0f1fb0a8314fca1c1655f822b338
Sharon Naim <sharonthecreator@gmail.com>
2026-03-03 11:44:52 +0200

    feat: refactored the distribution of buffers to child nodes.
    Optimized the buffer cloning and refactor logic for distribution,
    now will pass by ref to all readonly, but also in general the last child
    node - no matter if it's readonly/readwrite.
    
    Signed-off-by: Sharon Naim <sharonthecreator@gmail.com>

--- subsys/zstreamer/node.c
+++ subsys/zstreamer/node.c
@@ -14,52 +14,29 @@
 LOG_MODULE_REGISTER(zstreamer_node, CONFIG_ZSTREAMER_LOG_LEVEL);
 
 /* ------------------------------------------------------------------ */
-/* Shared helpers                                                      */
+/* Shared helpers                                                     */
 /* ------------------------------------------------------------------ */
 
 void zstreamer_node_distribute(const struct device *dev, struct net_buf *buf,
                                const struct device *const *children,
                                size_t num_children) {
-  const struct zstreamer_node_config *cfg =
-      (const struct zstreamer_node_config *)dev->config;
   struct zstreamer_node_data *child_data = NULL;
   const struct zstreamer_node_config *child_cfg = NULL;
-
-  if (num_children == 0) {
-    net_buf_unref(buf);
-    return;
-  }
-
-  /* Single stream optimization. */
-  if (buf->ref == 1 && num_children == 1) {
-    child_data = (struct zstreamer_node_data *)children[0]->data;
-    k_fifo_put(&child_data->fifo, buf);
-    return;
-  }
-
-  bool shareable_buffer = !cfg->readonly;
-  if (!cfg->readonly) {
-    for (size_t i = 0; i < num_children; i++) {
-      child_cfg = (const struct zstreamer_node_config *)children[i]->config;
-
-      if (child_cfg->readonly) {
-        shareable_buffer = false;
-        break;
-      }
-    }
-  }
 
   for (size_t i = 0; i < num_children; i++) {
     child_data = (struct zstreamer_node_data *)children[i]->data;
     child_cfg = (const struct zstreamer_node_config *)children[i]->config;
 
-    if (child_cfg->readonly || shareable_buffer) {
+    /* Pass the buffer directly if the child node is readonly,
+     * or if it's the last child and the buffer has only one reference. */
+    if (child_cfg->readonly || (buf->ref == 1 && i == (num_children - 1))) {
       buf = net_buf_ref(buf);
       k_fifo_put(&child_data->fifo, buf);
-      shareable_buffer = false;
-    } else {
+    }
+    /* Clone the buffer for a readwrite child, unless it's the last one and
+     * the buffer hasn't been shared. */
+    else {
       struct net_buf *clone = net_buf_clone(buf, K_NO_WAIT);
-
       if (clone != NULL) {
         k_fifo_put(&child_data->fifo, clone);
       } else {
